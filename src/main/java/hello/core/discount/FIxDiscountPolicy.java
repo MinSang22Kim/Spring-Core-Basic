e18b5b6f8f5f82766b2a197ba302afe4d735abe7
MinSang22Kim <powerminsang9901@gmail.com>
2024-03-24 12:07:25 +0900

    주문과 할인 도메인 실행과 테스트

---
+++ src/main/java/hello/core/discount/FIxDiscountPolicy.java
@@ -0,0 +1,18 @@
+package hello.core.discount;
+
+import hello.core.member.Gradle;
+import hello.core.member.Member;
+
+public class FIxDiscountPolicy implements DiscountPolicy{
+
+    private int discountFixAmount = 1000; // 1000원 할인
+
+    @Override
+    public int discount(Member member, int price) {
+        if(member.getGrade() == Gradle.VIP){
+            return discountFixAmount;
+        } else {
+            return 0;
+        }
+    }
+}
