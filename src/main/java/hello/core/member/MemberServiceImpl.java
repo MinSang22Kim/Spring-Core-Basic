d87ea6ad0e98ad3dc2b4441fcff60b4e4eb58a3d
MinSang22Kim <powerminsang9901@gmail.com>
2024-03-26 19:32:28 +0900

    @Configuration과 싱글톤

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -17,4 +17,9 @@ public class MemberServiceImpl implements MemberService{
     public Member findMember(Long memberId) {
         return memberRepository.findById(memberId);
     }
+
+    // 테스트 용도
+    public MemoryMemberRepository getMemberRepository() {
+        return memberRepository;
+    }
 }
